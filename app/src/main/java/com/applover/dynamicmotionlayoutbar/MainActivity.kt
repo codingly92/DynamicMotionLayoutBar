88b960a3be93a044fd3049e60b46d662881741d3
Janusz Hain <janusz.hain@applover.com>
2022-08-17 14:51:39 +0200

    Add toggling between examples

--- app/src/main/java/com/applover/dynamicmotionlayoutbar/MainActivity.kt
+++ app/src/main/java/com/applover/dynamicmotionlayoutbar/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.applover.dynamicmotionlayoutbar
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.applover.dynamicmotionlayoutbar.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var availableScreens = Example.values().iterator().also {
+        it.next()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +23,23 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            toggleExample(view)
         }
 
         setupFirstScreen()
+    }
+
+    private fun toggleExample(view: View) {
+        if (!availableScreens.hasNext()) {
+            availableScreens = Example.values().iterator()
+        }
+        val currentScreen = availableScreens.next()
+        changeVisibilityForExamples(currentScreen)
+        Snackbar.make(view, currentScreen.name, Snackbar.LENGTH_LONG).show()
+    }
+
+    private fun changeVisibilityForExamples(example: Example) {
+        binding.layout.root.isVisible = example == Example.ActivableImageScreen
     }
 
     private fun setupFirstScreen() = with(binding.layout) {
@@ -33,5 +50,9 @@ class MainActivity : AppCompatActivity() {
         buttonDeactivate.setOnClickListener {
             imageViewProfile.setActive(false)
         }
+    }
+
+    enum class Example {
+        ActivableImageScreen, StepProgressBar, StepProgressBarTwo,
     }
 }
