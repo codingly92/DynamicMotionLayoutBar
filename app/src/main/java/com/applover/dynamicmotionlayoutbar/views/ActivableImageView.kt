7d30551663543230eafb110556d88012cb83a498
Janusz Hain <janusz.hain@applover.com>
2022-08-20 01:22:43 +0200

    Add comments for TwoStateMotionLayout and ActivableImageView

--- app/src/main/java/com/applover/dynamicmotionlayoutbar/views/ActivableImageView.kt
+++ app/src/main/java/com/applover/dynamicmotionlayoutbar/views/ActivableImageView.kt
@@ -16,6 +16,9 @@ import com.applover.dynamicmotionlayoutbar.utils.TwoStateMotionLayout
 import com.applover.dynamicmotionlayoutbar.utils.centerInParent
 import com.applover.dynamicmotionlayoutbar.utils.createConstraintSet
 
+/**
+ * Example view that animated tint change for imageView
+ */
 @Suppress("SpellCheckingInspection")
 class ActivableImageView @JvmOverloads constructor(
     context: Context,
@@ -27,20 +30,29 @@ class ActivableImageView @JvmOverloads constructor(
     private var inactiveTint: Int = -1
     private var drawableRes: Int = -1
 
+    /**
+     * Initialize from code and get attributes for colors of icons, animation duration and initial state
+     * Note that we need to call setupView as onFinishInflate in TwoStateMotionLayout will not be called as it was not created in XML
+     */
     constructor(
         context: Context,
         @DrawableRes drawableRes: Int,
         @ColorRes activeTint: Int,
         @ColorRes inactiveTint: Int,
         animationDuration: Int,
+        startAtTheEndTransition: Boolean = false,
     ) : this(context, null, 0) {
         this.drawableRes = drawableRes
         this.activeTint = ContextCompat.getColor(context, activeTint)
         this.inactiveTint = ContextCompat.getColor(context, inactiveTint)
         this.animationDuration = animationDuration
+        this.startAtTheEndTransition = startAtTheEndTransition
         setupView()
     }
 
+    /**
+     * Initialize from xml and get attributes for colors of icons, animation duration and initial state
+     */
     init {
         attrs?.let {
             context.theme.obtainStyledAttributes(
@@ -62,6 +74,9 @@ class ActivableImageView @JvmOverloads constructor(
         }
     }
 
+    /**
+     * We need to create views first and then set constraints for initial state and end state of animation
+     */
     override fun ConstraintLayout.createViewsAndConstraintSets(): Pair<ConstraintSet, ConstraintSet> {
         val imageView = ImageView(context)
         val imageViewId = generateViewId()
@@ -85,6 +100,9 @@ class ActivableImageView @JvmOverloads constructor(
         return startSet to endSet
     }
 
+    /**
+     * The easiest way to manipulate single transation is transitionToEnd and transitionToStart as we don't need any ids of constraints nor transitions
+     */
     fun setActive(isActive: Boolean) {
         if (isActive) {
             transitionToEnd()
